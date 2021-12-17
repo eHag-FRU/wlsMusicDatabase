0c33fe8b936fbd2737ac8f9ddfda043678bfa6d2
eHag-FRU <hager10@email.franklin.edu>
2021-12-17 17:37:47 -0500

    CodeIgniter 4 Base with some minor issues when using Ammp along side.
    Currently working on the main view that will be the search form.

---
+++ app/Views/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>Test</title>
+
+		<link rel="stylesheet" href="CSS/index.css">
+	</head>
+
+	<body>
+		<div class="mainGrid">
+			<header class="header">
+		
+				<h1>This is a test header</h1>
+				
+			</header>
+
+			<nav class="Side" id="Side">
+				<ul>
+
+					<li>This is a test item</li>
+
+				</ul>
+
+
+			</nav>
+
+			<form class="Search" id="Search">
+				<h1>This is a test Form</h1>
+			<form>
+
+			<nav class="EndSide" id="EndSide">
+				<ul>
+
+					<li>This is a test item</li>
+
+				</ul>
+
+
+			</nav>
+
+
+			<footer class="Footer" id="Footer">
+				<h2>This is a test footer</h2>
+			</Footer>
+		</div>
+	</body>
+
+</html>
