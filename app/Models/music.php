53d65b8e8f288ac7f4cbae95731c7cff0a7b5fae
eHag-FRU <hager10@email.franklin.edu>
2021-12-21 01:27:21 -0500

    Changed the server to XAMMP due to ease of use and the way it plays nicely with CodeIgniter when compared to AMPPS.
    Started to use phpMyAdmin to make the database with some sample pieces of music from marching season (ex. Crazy Train, Livin' On A Prayer, etc.).
    
    I am close to tackling the models portion of the code. I have configured the database connection.

---
+++ app/Models/music.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UserModel extends Model {
+    
+    protected $table = 'pieces';
+    protected $primarykey = 'ID';
+
+
+    public function search() {
+
+    }
+
+}
