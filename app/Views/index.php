86c33853464cf1f15090d812ed66808ebaafa6ff
eHag-FRU <hager10@email.franklin.edu>
2022-04-14 09:58:12 -0400

    Made the search form into two rows of input and made all input W3.CSS input

--- app/Views/index.php
+++ app/Views/index.php
@@ -9,16 +9,15 @@
 				<form action="/Search" method="POST">
 
 					
-					
-					
-					<label for="Title">Title:</label>
+					<div class="w3-third">
+						<label for="Title">Title:</label>
 					<input type="text" class="Title w3-input" id="Title" name="form[title]" placeholder="Crazy Train" default='NULL'>
 
 					<br>
 					<br>
 
 					<label for="Type">Piece Type: </label>
-					<select class="Type" name="form[type]" required>
+					<select class="Type w3-select" name="form[type]" required>
 						<option	value="Concert">Concert</option>
 
 						<option value="Marching">Marching/Pep</option>
@@ -28,30 +27,42 @@
 					<br>
 
 					<label for="Arranger">Arranger</label>
-					<input type="text" class='Arranger' name="form[Arranger]" default='NULL'>
+					<input type="text" class='Arranger w3-input' name="form[Arranger]" default='NULL'>
 
 					<br>
 					<br>
-						
-					<label for="Composer">Composer:</label>
-					<input type="text" class="Composer" id="Composer" name="form[composer]" placeholder="Crazy Train" default='NULL'>
+					</div>
+					
+					
+					<div class="w3-third">
+						<label for="Composer">Composer:</label>
+					<input type="text" class="Composer w3-input" id="Composer" name="form[composer]" placeholder="Crazy Train" default='NULL'>
 
 					<br>
 					<br>
 						
 					<label for="LibNumber">Music Library Number:</label>
-					<input type="number" class="LibNumber" id="LibNumber" name="form[LibNumber]" default='NULL'>
+					<input type="number" class="LibNumber w3-input" id="LibNumber" name="form[LibNumber]" default='NULL'>
 
 					<br>
 					<br>
 						
 					<label for="YearLastPlayed">Year Last Played:</label>
-					<input type="text" maxlength="4" minlength="4" class="YearLastPlayed" id="YearLastPlayed" name="form[YearLastPlayed]" default='NULL'>
+					<input type="text" maxlength="4" minlength="4" class="YearLastPlayed w3-input" id="YearLastPlayed" name="form[YearLastPlayed]" default='NULL'>
 
 
-					<p><em>*A blank search will search for everything</em></p>
+					</div>
+					
+					
+						
+					
+					<span>
+						<p><em>*A blank search will search for everything</em></p>
 
-					<br><button type="submit" class="w3-button w3-orange w3-margin" style="float: right; ">Submit</button><br>
+						<br><button type="submit" class="w3-button w3-orange w3-margin" style="float: right; ">Submit</button><br>
+					</span>
+
+					
 	
 				</form>				
 			</main>
