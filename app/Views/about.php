3e357eead19ba990b1daff635f8695717ff22679
eHag-FRU <hager10@email.franklin.edu>
2022-07-11 00:53:56 -0400

    Added in information to the about page and added a style sheet to complement

---
+++ app/Views/about.php
@@ -0,0 +1,36 @@
+<!-- Styles the add page with out the music note -->
+<link rel="stylesheet" type="text/css" href="<?= base_url('assets/CSS/about.css') ?>">
+
+    <img src="/treble.jpg" alt="" class="Image">
+
+	<main class="About">
+		<h2>About</h2>
+
+        <br>
+
+        <h3>Dedicated to Mr.Fannin's almost 10 years at West Liberty-Salem Schools as the 5-12 band director</h3>
+
+        <p>
+            A CodeIgniter 4 project for my local high school. The band program has hundreds of pieces spread accross the concert and marching band seasons. 
+            The current way the music library is, a print out of an outdated spreadsheet in which the source file has been lost, is ineffiecent. 
+            Due to not having the source file, I will be entering all of the pieces by hand. While this will take a while to accomplish, 
+            it is worth the extra time in the setup for a less painful enduser experience when deployed. <br><br>
+
+
+            My PHP skills learned in Franklin University's WEBD-263: Web Infromation Systems course are being pushed and expanded upon when working on this project. I had not used a PHP framework, so this is helpping me learn CodeIgniter along the way too.
+            <br>
+            <br>
+
+            Languages, Frameworks, and Server Software Used:
+            <ul>
+                <li>PHP 8</li>
+                <li>HTML 5</li>
+                <li>CSS 3</li>
+                <li>CodeIgniter 4</li>
+                <li>XAMPP Web Server</li>
+                <li>SQLServerBooster (For database backups on a sechdule of once a month at midnight)</li>
+            </ul>
+        </p>
+
+        
+	</main>
